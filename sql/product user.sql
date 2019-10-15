3a263b3f2d3450761071c34320d146a517511cd0
Your Name <you@example.com>
2019-10-15 13:14:07 +0700

    một vài bước cơ bản

---
+++ sql/product user.sql
@@ -0,0 +1,90 @@
+CREATE DATABASE Seafoody;
+
+CREATE TABLE User (
+	uid int auto_increment not null unique,
+    nickname varchar(255),
+    created_at timestamp,
+    gender enum("Male","Female"),
+    location varchar(255),
+    phoneNumber varchar(255),
+    email varchar(255),
+    avatarUrl varchar(255),
+    primary key (uid)
+);
+
+CREATE TABLE Company (
+    companyId int auto_increment not null unique,
+    companyName varchar(255),
+    avatarUrl varchar(255),
+	location varchar(255),
+    description varchar(255),
+    phoneNumber varchar(255),
+    primary key (companyId)
+);
+
+CREATE TABLE Promotion (
+	promotionId int auto_increment not null unique,user
+    promocode varchar(20),
+    promoPercent smallint,
+    exprireDate timestamp,
+    promoDescription varchar(255),
+    primary key (promotionId)
+);
+
+CREATE TABLE Product(
+    id int auto_increment not null unique,
+    productName varchar(255),
+    price numeric(10,2),
+    productType enum("Dong lanh","Tuoi song","Do kho"),
+    posted_at timestamp,
+    company int,
+    description varchar(255),
+    promotion int,
+    primary key (id),
+    foreign key (company) references Company(companyId),
+    foreign key (promotion) references Promotion(promotionId)
+);
+
+
+CREATE TABLE FeedbackOnProduct(
+    fopId int auto_increment not null unique,
+    user int,
+    product int,
+    fopText varchar(255),
+    likes int,
+    primary key (fopId),
+	created_at timestamp,
+    foreign key (user) references User(uid),
+    foreign key (product) references Product(id)
+);
+
+CREATE TABLE FeebackOnSite(
+    fosId int auto_increment not null unique,
+	fosType enum("Bao cao","Gop y"),
+    user int,
+    fosText varchar(255),
+	created_at timestamp,
+    primary key(fosId),
+    foreign key (user) references User(uid)
+);
+
+CREATE TABLE ProductImages (
+	imageId int auto_increment not null unique,
+    imageUrl varchar(255),
+    feature boolean,
+    product int,
+    primary key (imageId),
+    foreign key (product) references Product(id)
+);
+
+-- Favorites for products
+CREATE TABLE Favorites (
+    favorId int auto_increment not null unique,
+    product int,
+    customer int,
+	primary key (favorId),
+    foreign key (customer) references User(uid),
+    foreign key (product) references Product(id)
+);
+
+
